d6cc05c8aa28480713363d6b90e33a9b8b7f591d
bigbangifbav2 <bigbangifbav2@gmail.com>
2026-02-09 20:50:33 -0300

    Update AdminElementosPage.tsx

--- frontend/src/pages/AdminElementosPage/AdminElementosPage.tsx
+++ frontend/src/pages/AdminElementosPage/AdminElementosPage.tsx
@@ -30,7 +30,7 @@ const AdminElementosPage: React.FC = () => {
 
     // Estados de Filtro
     const [busca, setBusca] = useState('');
-    const [nivelFiltro, setNivelFiltro] = useState('TODOS'); // <--- NOVO
+    const [nivelFiltro, setNivelFiltro] = useState('TODOS');
 
     const [elementos, setElementos] = useState<Elemento[]>([]);
     const [loading, setLoading] = useState(true);
@@ -306,5 +306,6 @@ const AdminElementosPage: React.FC = () => {
         </div>
     );
 };
+
 
 export default AdminElementosPage;
